c78a7392401f05d1faba699760558e4e56540490
william.d.zk <william_duckular@msn.com>
2018-09-04 07:35:09 +0800

    bug fix

--- Z-Queen/src/main/java/com/tgx/z/queen/io/external/websokcet/bean/control/X101_HandShake.java
+++ Z-Queen/src/main/java/com/tgx/z/queen/io/external/websokcet/bean/control/X101_HandShake.java
@@ -35,7 +35,7 @@ public class X101_HandShake
     public final static int COMMAND = 0x101;
 
     public X101_HandShake(String host, String secKey, int version) {
-        super(String.format("GET /ws_service HTTP/1.1\r\nHost: %s\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: %s\r\nOrigin: http://%s\r\nSec-WebSocket-Protocol: z-push, z-chat\r\nSec-WebSocket-Version: %s\r\n",
+        super(String.format("GET /ws_service HTTP/1.1\r\nHost: %s\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: %s\r\nOrigin: http://%s\r\nSec-WebSocket-Protocol: z-push, z-chat\r\nSec-WebSocket-Version: %s\r\n\r\n",
                             host,
                             secKey,
                             host,
