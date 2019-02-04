496b402419cfd8af033bbdf80c19547258d72a9f
william.d.zk <william@tgxstudio.com>
2019-02-04 15:30:58 +0800

    zuid 的属性文件放入正确的地方

--- update_pom.sh
+++ update_pom.sh
@@ -2,9 +2,9 @@
 version="1.0.0-SNAPSHOT"
 for file in $(find \./Z-* -name "pom.xml");do
      echo $file
-     sed -e "9s/<version>\(.*\)<\/version>/<version>${version}<\/version>/g$h" \
+     sed -e "33s/<version>\(.*\)<\/version>/<version>${version}<\/version>/g$h" \
      $file > $file.new
      mv -f $file.new $file
 done
-sed -e "10s/<version>\(.*\)<\/version>/<version>${version}<\/version>/g$h" ./pom.xml > ./pom.xml.new
+sed -e "34s/<version>\(.*\)<\/version>/<version>${version}<\/version>/g$h" ./pom.xml > ./pom.xml.new
 mv -f ./pom.xml.new ./pom.xml
