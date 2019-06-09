37d7e0b23384367598bf7571fbeebd56c01c42fa
william.d.zk <william_duckular@msn.com>
2019-06-09 13:57:37 +0800

    MQTT connect bug fix

--- Z-King/src/main/java/com/tgx/chess/king/base/util/IoUtil.java
+++ Z-King/src/main/java/com/tgx/chess/king/base/util/IoUtil.java
@@ -207,24 +207,25 @@ public interface IoUtil
     static byte[] variableLength(int length)
     {
         if (length == 0) return new byte[] { 0 };
-        int resLength = 0;
-        int result = 0;
-        do {
-            result |= (length & 0x7F) << 24;
-            length >>>= 7;
-            resLength++;
-            if (length > 0) {
-                result >>>= 8;
-                result |= 0x80000000;
-            }
+        if (length < 128) {
+            return new byte[] { (byte) length };
         }
-        while (length > 0);
-        byte[] res = new byte[resLength];
-        for (int i = 0, move = 24; i < resLength; i++) {
-            res[i] = (byte) (result >>> move);
-            move -= 8;
+        else if (length < 16384) {
+            return new byte[] { (byte) (0x80 | (length & 0x7F)),
+                                (byte) (length >>> 7) };
         }
-        return res;
+        else if (length < 2097152) {
+            return new byte[] { (byte) (0x80 | (length & 0x7F)),
+                                (byte) (0x80 | (length & 0x7F80) >>> 7),
+                                (byte) (length >>> 14) };
+        }
+        else if (length < 268435456) {
+            return new byte[] { (byte) (0x80 | (length & 0x7F)),
+                                (byte) (0x80 | (length & 0x7F80) >>> 7),
+                                (byte) (0x80 | (length & 0x3FC000) >>> 14),
+                                (byte) (length >>> 21) };
+        }
+        throw new ArrayIndexOutOfBoundsException("malformed length");
     }
 
     static byte[] variableLength(long length)
@@ -279,6 +280,20 @@ public interface IoUtil
             if ((cur & 0x80) != 0) length <<= 7;
         }
         while ((cur & 0x80) != 0 && buf.hasRemaining());
+        return length;
+    }
+
+    static int readVariableIntLength(ByteBuffer buf)
+    {
+        int length = 0;
+        int cur, pos = 0;
+        if (buf.hasRemaining()) do {
+            cur = buf.get();
+            length += (cur & 0x7F) << (pos * 7);
+            pos++;
+        }
+        while ((cur & 0x80) != 0 && buf.hasRemaining());
+
         return length;
     }
 
