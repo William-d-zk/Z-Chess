16bd0aa0661bf16a55c23a4362caaf0c8da932a9
william.d.zk <william@tgxstudio.com>
2020-05-29 00:19:51 +0800

    enable remote debug

--- consensus-start.sh
+++ consensus-start.sh
@@ -2,4 +2,4 @@
 git pull
 mvn clean install -Dmaven.test.skip=true
 cd Z-Consensus-Start
-mvn spring-boot:run
+mvn spring-boot:run -Ddev=true "-Dspring-boot.run.jvmArguments=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000"
