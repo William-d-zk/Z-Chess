a0e6abd4057d0fefec9f3d9a94739d8d119cea1d
william.d.zk <william@tgxstudio.com>
2020-05-30 23:11:13 +0800

    update

--- Z-Endpoint-Start/src/main/java/com/tgx/chess/pawn/start/ApplicationPawn.java
+++ Z-Endpoint-Start/src/main/java/com/tgx/chess/pawn/start/ApplicationPawn.java
@@ -34,7 +34,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = { "com.tgx.chess.knight.json",
                                             "com.tgx.chess.knight.raft",
                                             "com.tgx.chess.pawn.endpoint",
-"com.tgx.chess.open.api"})
+                                            "com.tgx.chess.open.api" })
 public class ApplicationPawn
 {
     public static void main(String[] args)
