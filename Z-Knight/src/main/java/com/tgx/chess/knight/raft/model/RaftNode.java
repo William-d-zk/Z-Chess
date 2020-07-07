e28991ccb00b2c3a6a1b698b1deeccc69fd8edf6
william.d.zk <william@tgxstudio.com>
2020-07-07 13:37:51 +0800

    update

--- Z-Knight/src/main/java/com/tgx/chess/knight/raft/model/RaftNode.java
+++ Z-Knight/src/main/java/com/tgx/chess/knight/raft/model/RaftNode.java
@@ -132,7 +132,7 @@ public class RaftNode<M extends IClusterPeer & IClusterTimer>
 
     public void init() throws IOException
     {
-        if (!_RaftConfig.isClusterMode()) {
+        if (!_RaftConfig.isClusterModel()) {
             _Logger.info("single model skip init raft node");
             return;
         }
@@ -969,7 +969,7 @@ public class RaftNode<M extends IClusterPeer & IClusterTimer>
 
     public boolean isClusterMode()
     {
-        return _RaftConfig.isClusterMode();
+        return _RaftConfig.isClusterModel();
     }
 
     @Override
