4430748db8423e9614411f110ac826e8ac261531
william.d.zk <william@tinygamex.com>
2020-10-02 01:23:31 +0800

    update copy right

--- Z-Knight/src/main/java/com/tgx/chess/knight/raft/model/log/Snapshot.java
+++ Z-Knight/src/main/java/com/tgx/chess/knight/raft/model/log/Snapshot.java
@@ -15,7 +15,7 @@
  *                                                                               
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR    
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,      
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE   
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
@@ -44,27 +44,32 @@ public class Snapshot
 {
 
     @Override
-    public long primaryKey() {
+    public long primaryKey()
+    {
         return 0;
     }
 
     @Override
-    public Operation operation() {
+    public Operation operation()
+    {
         return null;
     }
 
     @Override
-    public Strategy strategy() {
+    public Strategy strategy()
+    {
         return null;
     }
 
     @Override
-    public int serial() {
+    public int serial()
+    {
         return 0;
     }
 
     @Override
-    public long getOrigin() {
+    public long getOrigin()
+    {
         return 0;
     }
 }
