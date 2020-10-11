3ae14c47b10f7259ec6f298ef840ae0de1d8ee25
Idempotent <zhangzhuo.zhangzhu@cainiao.com>
2020-10-11 20:49:46 +0800

    update pom

--- Z-King/src/main/ntru/com/securityinnovation/jNeo/ntruencrypt/encoder/NtruEncryptKeyNativeEncoder.java
+++ Z-King/src/main/ntru/com/securityinnovation/jNeo/ntruencrypt/encoder/NtruEncryptKeyNativeEncoder.java
@@ -39,6 +39,7 @@ public class NtruEncryptKeyNativeEncoder implements NtruEncryptKeyEncoder
     public final static byte PUBLIC_KEY_v1 = 1;
 
 
+
     /**
      * The format for the private key blob where the private
      * key is represented by the trinomial F, packed 5 trits per
@@ -53,6 +54,7 @@ public class NtruEncryptKeyNativeEncoder implements NtruEncryptKeyEncoder
      *              F[5*i+3]*3 + F[5*i+4]
      */
     public final static byte PRIVATE_KEY_DEFAULT_v1 = 2;
+
 
 
     /**
