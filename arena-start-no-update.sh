e9d172acb34cd90b0cb4b0c150e095d4112bfdf3
william.d.zk <william@isahl.com>
2021-05-26 02:42:36 +0800

    后台执行，git pull 需要手动知行，github的成功率比较低

--- arena-start-no-update.sh
+++ arena-start-no-update.sh
@@ -1,3 +1,3 @@
 mvn clean install -Dmaven.test.skip=true -P run
 cd Z-Arena || exit
-mvn spring-boot:run -Dspring-boot.run.profiles=daily "-Dspring-boot.run.jvmArguments=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000"
+nohup mvn spring-boot:run -Dspring-boot.run.profiles=daily "-Dspring-boot.run.jvmArguments=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000" &
