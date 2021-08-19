f43bb71b227c1d0c06eb2af35ece97ae0b14b232
william.d.zk <william@isahl.com>
2021-08-19 19:14:41 +0800

    update copyright

--- Z-Knight/src/test/java/TestAccountAuth.java
+++ Z-Knight/src/test/java/TestAccountAuth.java
@@ -1,9 +1,9 @@
 
 /*
  * MIT License
- * 
- * Copyright (c) 2016~2020. Z-Chess
- * 
+ *
+ * Copyright (c) 2016~2021. Z-Chess
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
  * the Software without restriction, including without limitation the rights to
