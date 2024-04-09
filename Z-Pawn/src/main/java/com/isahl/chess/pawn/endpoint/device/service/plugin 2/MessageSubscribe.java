76c0d5f3ab20ba39ad8c74aaa8727a1ab88132b5
william.d.zk <william_duckular@msn.com>
2024-04-09 12:43:49 +0800

    Message 写入DB时需要生成summary

---
+++ Z-Pawn/src/main/java/com/isahl/chess/pawn/endpoint/device/service/plugin 2/MessageSubscribe.java
@@ -0,0 +1,56 @@
+/*
+ *  MIT License
+ *
+ * Copyright (c) 2024.  Z-Chess
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy of
+ *   this software and associated documentation files (the "Software"), to deal in
+ *   the Software without restriction, including without limitation the rights to
+ *   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ *   the Software, and to permit persons to whom the Software is furnished to do so,
+ *   subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ *   FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ *   COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ *   IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package com.isahl.chess.pawn.endpoint.device.service.plugin;
+
+import com.isahl.chess.king.base.features.model.IoSerial;
+import com.isahl.chess.king.base.log.Logger;
+import com.isahl.chess.pawn.endpoint.device.db.central.model.MessageEntity;
+import com.isahl.chess.pawn.endpoint.device.resource.features.IMessageService;
+import com.isahl.chess.pawn.endpoint.device.spi.plugin.PersistentHook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class MessageSubscribe
+        implements PersistentHook.ISubscribe
+{
+    private final Logger          _Logger = Logger.getLogger("endpoint.pawn." + getClass().getSimpleName());
+    private final IMessageService _MessageService;
+
+    @Autowired
+    public MessageSubscribe(IMessageService messageService) {_MessageService = messageService;}
+
+    @Override
+    public void onBatch(List<IoSerial> contents)
+    {
+        _Logger.debug("message subscribe batch");
+        _MessageService.submitAll(contents.stream()
+                                          .filter(c->c instanceof MessageEntity)
+                                          .map(c->(MessageEntity) c)
+                                          .toList());
+    }
+}
