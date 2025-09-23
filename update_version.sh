05514457fbae5f7e943cbd7e570f80e54ab20222
william.d.zk <william_duckular@msn.com>
2025-09-23 11:39:06 +0800

    Z-chess release 1.0.18

--- update_version.sh
+++ update_version.sh
@@ -21,7 +21,7 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
-version="1.0.17"
+version="1.0.18"
 # shellcheck disable=SC2044
 for file in $(find \./Z-* -name "pom.xml"); do
   echo $file
